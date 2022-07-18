3df28f25e84a71416e3c3763464cfa53a2efc49e
Immortal813 <rostimmortal@yandex.ru>
2022-07-18 12:55:01 +0300

    q

--- First/First/main.cpp
+++ First/First/main.cpp
@@ -1,17 +1,23 @@
 ﻿#include <iostream>
 #include "getData_class.h"
 #include "setData_class.h"
-#include <vector>
-#include <thread>
 #include <string>
-#include <mutex>
-#include <queue>
+#include <Windows.h>
+#include <shellapi.h>
+
+void start( std::string qwe ) {
+
+	ShellExecuteA ( NULL, NULL, qwe.c_str(), NULL, NULL, SW_MAXIMIZE );
+
+}
 
 
 int main() {
 
 
+	std::cout << "Start programm" << std::endl;
 
+	start( "mspaint" );
 
 	return 0;
 
